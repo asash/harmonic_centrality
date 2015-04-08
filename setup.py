4e1df917ee7b74821d823e85a66dc4938840a2da
Alexander Petrov <a.petrov@datacentric.ru>
2015-04-08 12:17:04 +0300

    make pip-compatible

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='Harmonic Centrality',
+setup(name='harmonic_centrality',
       version='0.1',
        description='Harmonic Centrality metric realization for networkx',
        author='Alexander Petrov',
        author_email='firexel@gmail.com',
        url='https://github.com/asash/harmonic_centrality',
        packages=['harmonic_centrality'],
+       install_requires = [
+              'networkx',
+              'HLL'],
       )
 
